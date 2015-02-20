b4b0997daff21963603b1e0c82a8a6676e710015
Chad Williamson <chad@dahc.us>
2015-02-20 00:51:01 -0600

    unit test for ZobristTable serialization

--- src/test/java/us/dahc/goliphant/go/hashing/ZobristTableTest.java
+++ src/test/java/us/dahc/goliphant/go/hashing/ZobristTableTest.java
@@ -2,22 +2,37 @@ package us.dahc.goliphant.go.hashing;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.commons.lang3.SerializationUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 public class ZobristTableTest {
 
-    private ZobristTable zobTab;
+    private ZobristTable zobristTable;
 
     @Before
     public void setup() {
-        zobTab = new ZobristTable(new long[2][13][21]);
+        Random rand = new Random();
+        long[][][] table = new long[2][13][21];
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 13; j++)
+                for (int k = 0; k < 21; k++)
+                    table[i][j][k] = rand.nextLong();
+        zobristTable = new ZobristTable(table);
     }
 
     @Test
     public void sizeTest() {
-        assertEquals(13, zobTab.getRows());
-        assertEquals(21, zobTab.getColumns());
+        assertEquals(13, zobristTable.getRows());
+        assertEquals(21, zobristTable.getColumns());
+    }
+
+    @Test
+    public void serializationTest() {
+        byte[] serializedZobristTable = SerializationUtils.serialize(zobristTable);
+        assertEquals(zobristTable, SerializationUtils.deserialize(serializedZobristTable));
     }
 
 }

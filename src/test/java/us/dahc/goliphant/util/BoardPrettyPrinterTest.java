f28b1f3af92c4d4c6bb0e30148fcdc707e8054fc
Chad Williamson <chad@dahc.us>
2015-02-22 22:35:51 -0600

    pretty printer unit test (mostly about not crashing on weird sizes)

---
+++ src/test/java/us/dahc/goliphant/util/BoardPrettyPrinterTest.java
@@ -0,0 +1,25 @@
+package us.dahc.goliphant.util;
+
+import org.junit.Test;
+import us.dahc.goliphant.go.Board;
+import us.dahc.goliphant.go.DefaultBoard;
+
+import java.util.Random;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+
+public class BoardPrettyPrinterTest {
+
+    @Test
+    public void testPrettyPrintStability() throws Exception {
+        Board board = new DefaultBoard(new ZobristTable(new Random()));
+        String prettyBoard = BoardPrettyPrinter.getPrettyString(board);
+        assertThat("actually wrote board", prettyBoard.length(), is(greaterThan(800)));
+        board.resize(50, 25);
+        String prettyHugeBoard = BoardPrettyPrinter.getPrettyString(board);
+        assertThat("actually wrote big board", prettyHugeBoard.length(), is(greaterThan(2500)));
+    }
+
+}
